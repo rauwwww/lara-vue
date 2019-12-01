084501b1e8c3363337be656472ed2d462d52164a
Someone <Patrickholsths@gmail.com>
2019-12-01 22:48:39 +0100

    init setup

---
+++ README.md
@@ -0,0 +1,14 @@
+### Setup:
+```sh
+$ composer install --ignore-platform-reqs
+$ cp .env.example .env
+$ php artisan key:generate
+```
+#### Run mysql from docker:
+```
+$ docker-compose up
+```
+
+## Documentation:
+
+Auth/passport - https://laravel.com/docs/5.6/passport
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        \Laravel\Passport\Passport::ignoreMigrations();
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // DB::listen(function ($query) {
+        //     $query->sql;
+        //     // $query->bindings
+        //     $query->time;
+        // });
+    }
+}
