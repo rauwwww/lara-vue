87b78fdd0212fa38c910c347c46b7d21a7783b48
Someone <Patrickholsths@gmail.com>
2019-12-10 22:03:00 +0100

    feat: init api, controllers, model for cafes

---
+++ app/Http/Controllers/API/CafesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+
+
+class CafesController extends Controller
+{
+	/**
+	 * Get All cafes
+	 * URL: /api/v1/cafes
+	 * Method: GET
+	 * Description: Gets all of the cafes
+	 */
+	public function getCafes()
+	{ }
+
+	public function getCafe($id)
+	{ }
+
+	public function postNewCafe()
+	{ }
+}
