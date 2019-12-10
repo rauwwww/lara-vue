6878d179251c5dc39be9986200853ff3d0e58620
Someone <Patrickholsths@gmail.com>
2019-12-10 22:52:14 +0100

    feat: controller methods for cafes

--- app/Http/Controllers/API/CafesController.php
+++ app/Http/Controllers/API/CafesController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Models\Cafe;
 use App\Http\Controllers\Controller;
-
+use Illuminate\Http\Request;
 
 class CafesController extends Controller
 {
@@ -14,11 +15,31 @@ class CafesController extends Controller
 	 * Description: Gets all of the cafes
 	 */
 	public function getCafes()
-	{ }
+	{
+		$cafes = Cafe::all();
+
+		return response()->json($cafes);
+	}
 
 	public function getCafe($id)
-	{ }
+	{
+		$cafe = Cafe::where('id', '=', $id)->first();
+
+		return response()->json($cafe);
+	}
 
 	public function postNewCafe()
-	{ }
+	{
+		$cafe = new Cafe();
+
+		$cafe->name     = Request::get('name');
+		$cafe->address  = Request::get('address');
+		$cafe->city     = Request::get('city');
+		$cafe->state    = Request::get('state');
+		$cafe->zip      = Request::get('zip');
+
+		$cafe->save();
+
+		return response()->json($cafe, 201);
+	}
 }
